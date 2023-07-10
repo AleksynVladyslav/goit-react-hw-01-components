da5ef1dd5bd14e4c999e1478e3130c6a9356dd25
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-07-10 21:43:14 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,48 @@
-import css from './Profile.module.css';
-import { ProfileStats } from './ProfileStats';
-import { ProfileDescr } from './ProfileDescr';
+import propTypes from 'prop-types';
 
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+import css from './Profile.module.css';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
   return (
     <div className={css.profile}>
-      <ProfileDescr
-        username={username}
-        tag={tag}
-        location={location}
-        avatar={avatar}
-      />
-      <ProfileStats stats={stats} />
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers </span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views </span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes </span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>{' '}
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: propTypes.string.isRequired,
+  tag: propTypes.string.isRequired,
+  location: propTypes.string.isRequired,
+  avatar: propTypes.string.isRequired,
+  stats: propTypes.shape({
+    followers: propTypes.number.isRequired,
+    views: propTypes.number.isRequired,
+    likes: propTypes.number.isRequired,
+  }).isRequired,
 };
